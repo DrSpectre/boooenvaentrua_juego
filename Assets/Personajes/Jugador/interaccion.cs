95a11a309a14c2ece30f6400ff91e331ab12834b
Jadzia Eleonore <raccoonevef@gmail.com>
2025-12-19 17:39:59 -0700

    [AGREGA] las primeras partes de la interaccion con objetos

--- Assets/Personajes/Jugador/interaccion.cs
+++ Assets/Personajes/Jugador/interaccion.cs
@@ -1,15 +1,18 @@
 using System;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Interaccion: MonoBehaviour{
-    enum estados_interaccion {
+    enum EstadosInteraccion {
         inactivo,
         activo
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private PlayerInput entradas;
     private InputAction interactuar;
+    private EstadosInteraccion estado = EstadosInteraccion.inactivo;
+
     void Awake() {
         entradas = GetComponent<PlayerInput>();
 
@@ -18,9 +21,25 @@ public class Interaccion: MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        if(interactuar.ReadValue<float>() > 0){
+        if (estado == EstadosInteraccion.activo && interactuar.ReadValue<float>() > 0){
             Debug.Log("Se ha pulsado la tecla de itneractuar");
         }
+    }
 
+    void OllisionEnter(Collision collision){
+        Debug.Log($"Colision con {collision.collider.name}");
+        
+    }
+    
+    void OnTriggerEnter(Collider colision){
+        if(colision.gameObject.tag == "interactuable"){
+            estado = EstadosInteraccion.activo;
+        }
+    }
+
+    void OnTriggerExit(Collider colision){
+        if(colision.gameObject.tag == "interactuable"){
+            estado = EstadosInteraccion.inactivo;
+        }
     }
 }
---
+++ Assets/codigo/interfaces/interfaz_interaccion.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public interface InterfazInteraccion{
+    void activar();
+    void interactuar();
+    void agregar();
+}
